32599c6a67bd72fa92ada380c422840e6c15fa5d
simit <semihbiyikli769@gmail.com>
2025-07-31 14:49:40 +0200

    Update settings.py

--- PythonProject1/pyshop/settings.py
+++ PythonProject1/pyshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-**2c+7am(ldagd+yjb9ng3aswxo-hhqj)mg@!%d*1sxj34=eo3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
